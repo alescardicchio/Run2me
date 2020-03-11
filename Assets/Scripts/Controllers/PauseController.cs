7cf5f4545f1478cdfa5eb23ee4f4e29c3ef9ce99
Alessandro Scardicchio <ale.scardicchio@outlook.com>
2020-03-11 17:09:45 +0100

    Pause Menu aggiunto

---
+++ Assets/Scripts/Controllers/PauseController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    //public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    public void LoadMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quitting");
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}

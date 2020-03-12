26e5bc4c6bac875384bac2ceec09b860d2c45b3e
Alessandro Scardicchio <ale.scardicchio@outlook.com>
2020-03-12 23:17:48 +0100

    Update 12 Marzo
    + Fixato sistema delle vite
    + Il player muore sia quando viene ucciso da un nemico, sia quando manca una piattaforma e precipita

---
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score;
+    public int lifeScore;
+    public bool playerDied_GameRestarted;
+
+
+    void Awake() {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton() {
+        if(instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public void PlayGame() {
+        GameManager.instance.playerDied_GameRestarted = false;
         SceneManager.LoadScene("gameLvl1");
     }
 }

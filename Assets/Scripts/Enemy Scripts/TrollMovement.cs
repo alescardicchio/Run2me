e83229999af734137d537ea500a4a34c827b73d1
MariaCarmelaPascale <61779407+MariaCarmelaPascale@users.noreply.github.com>
2020-03-13 13:59:30 +0100

    add script trollWalk

--- Assets/Scripts/Enemy Scripts/TrollMovement.cs
+++ Assets/Scripts/Enemy Scripts/TrollMovement.cs
@@ -6,11 +6,11 @@ public class TrollMovement : MonoBehaviour
 {
 
     public float walkSpeed = 1.5f;
-    public bool walkLeft;
+    public bool walkLeft = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(changeDirection());
     }
 
     // Update is called once per frame
@@ -22,5 +22,26 @@ public class TrollMovement : MonoBehaviour
     private void trollWalk()
     {
         Vector3 temp = transform.position;
+        Vector3 tempScale = transform.localScale;
+        if(walkLeft)
+        {
+            temp.x += walkSpeed * Time.deltaTime;
+            tempScale.x = Mathf.Abs(tempScale.x);
+        }
+        else
+        {
+            temp.x -= walkSpeed * Time.deltaTime;
+            tempScale.x = -Mathf.Abs(tempScale.x);
+        }
+
+        transform.position = temp;
+        transform.localScale = tempScale;
+    }
+
+    IEnumerator changeDirection()
+    {
+        yield return new WaitForSeconds(2.5f);    // Attende un tempo pari a 2.5 secondi.
+        walkLeft = !walkLeft;   // Cambia direzione.
+        StartCoroutine(changeDirection());
     }
 }

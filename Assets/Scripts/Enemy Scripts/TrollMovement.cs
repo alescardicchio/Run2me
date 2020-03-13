ba6cb2b8b9d8b73ce4efcaa2b3f0e2b2b3365960
MariaCarmelaPascale <61779407+MariaCarmelaPascale@users.noreply.github.com>
2020-03-13 13:34:20 +0100

    modifiche

---
+++ Assets/Scripts/Enemy Scripts/TrollMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollMovement : MonoBehaviour
+{
+
+    public float walkSpeed = 1.5f;
+    public bool walkLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trollWalk();
+    }
+    
+    private void trollWalk()
+    {
+        Vector3 temp = transform.position;
+    }
+}

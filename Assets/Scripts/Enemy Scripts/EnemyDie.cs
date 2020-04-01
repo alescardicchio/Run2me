4642c2b910f6b8be85b04ed5e7afdc61597477c9
Alessandro Scardicchio <ale.scardicchio@outlook.com>
2020-04-01 20:37:39 +0200

    Knife Attack aggiunto

--- Assets/Scripts/Enemy Scripts/EnemyDie.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDie.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class EnemyDie : MonoBehaviour
 {
-    private Animator anim;
+    //private Animator anim;
     // Start is called before the first frame update
     void Awake()
     {
-        anim = GetComponent<Animator>();
+      //  anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     public void Die()
     {
-        anim.SetBool("Die", true);
-        transform.gameObject.tag = "Untagged";
-        Destroy(gameObject, 1.25f);
+        //anim.SetBool("Die", true);
+        //transform.gameObject.tag = "Untagged";
+        //Destroy(gameObject, 1.25f);
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Attack();
+        }    
+    }
+
+    public void Attack() {
+
+        // Avvia l'animazione
+        anim.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (var enemy in hitEnemies) {
+            enemy.GetComponent<EnemyDie>().Die();
+        }
+
+    }
+
+    void OnDrawGizmosSelected() {
+
+        if(attackPoint == null) {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}

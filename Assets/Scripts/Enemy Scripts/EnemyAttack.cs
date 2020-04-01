6f8ffc07a18b27dd70d0efd50e27f7a0d6b67bd5
MichelaPascale <61779452+MichelaPascale@users.noreply.github.com>
2020-04-01 22:06:57 +0200

    Added EnemyAttack
    Proprità inserite solamente nei due yeti

---
+++ Assets/Scripts/Enemy Scripts/EnemyAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    public static bool isAttacking = false;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("PlayerGirl"))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("PlayerGirl"))
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+
+}

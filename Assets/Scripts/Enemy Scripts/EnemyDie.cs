473162688da01e2dc7c33704e301284502adff1b
Alessandro Scardicchio <ale.scardicchio@outlook.com>
2020-04-02 13:14:44 +0200

    knife fixed

--- Assets/Scripts/Enemy Scripts/EnemyDie.cs
+++ Assets/Scripts/Enemy Scripts/EnemyDie.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class EnemyDie : MonoBehaviour
 {
-    //private Animator anim;
-    // Start is called before the first frame update
-    void Awake()
-    {
-      //  anim = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
     public void Die()
     {
-        anim.SetBool("Die", true);
-        transform.gameObject.tag = "Untagged";
         Destroy(gameObject);
     }
 }
